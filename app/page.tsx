f2d3606443526bfaeb0ef0a348845dec0d73af03
vorudev <vxruuu@gmaIl.com>
2025-10-23 12:06:29 +0000

    6th

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,11 @@
-import Image from "next/image";
-import { PlusIcon, UserPlusIcon } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { ProductForm} from "@/components/forms/product-form";
-import { getCategories } from "@/lib/actions/product-categories";
-import ProductsTable from "@/components/products-table";
-import { categories } from "@/db/schema";
+import Link from "next/link";
 
-import { getProducts } from "@/lib/actions/product";
-import { getAttributeCategories } from "@/lib/actions/attributes-categories";
 export default async function Home() {
-const categories = await getCategories(); // Fetch categories 
-const products = await getProducts();
-  const attributeCategories = await getAttributeCategories();
+
 
   return (
      <div className="container mx-auto p-4  ">
-
-      <h1 className="text-2xl font-bold mb-4">Dashboard</h1> 
-      <div className="flex justify-end">
-      <Dialog>
-  <DialogTrigger asChild><Button>Add Item<PlusIcon /></Button></DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Add Item</DialogTitle>
-      <DialogDescription>
-        Add a new item to the DataBase
-      </DialogDescription>
-      <ProductForm categories={categories}/>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
- 
-    </div>
-      <ProductsTable        initialProducts={products}
-      categories={categories}
-      attributeCategories={attributeCategories}
-/>
+      <Link href="/dashboard" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">зайти в панель администратора</Link>
     </div>
   );
 }
