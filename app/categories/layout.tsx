686f336d92e73686a4a755dd62204cb2de3c3716
Kirill Mokhov <vxruuu@gmaIl.com>
2025-11-07 21:46:31 +0300

    Update layout.tsx

--- app/categories/layout.tsx
+++ app/categories/layout.tsx
@@ -1,15 +1,17 @@
-
+import { Suspense } from "react";
 import Header from "@/components/frontend/header";
 import NavMenuMobile from "@/components/frontend/nav-menu-mobile";
 
 export default function CategoryLayout({ children }: { children: React.ReactNode }) {
     return (
     <>
+<Suspense fallback={<div>Loading...</div>}>
     <Header />
     <div className="bg-gray-100">
            
             {children}
         </div>
     <NavMenuMobile />
+</Suspense>
     </>
 )}
