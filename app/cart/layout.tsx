9f2e5d857e4961b64d0926353b32db6dba83f8a4
Kirill Mokhov <vxruuu@gmaIl.com>
2025-11-10 14:48:52 +0300

    params

--- app/cart/layout.tsx
+++ app/cart/layout.tsx
@@ -1,10 +1,13 @@
 import Header from "@/components/frontend/header";
 import NavMenuMobile from "@/components/frontend/nav-menu-mobile";
+import { Suspense } from "react";
 
 export default function CartLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
+            <Suspense fallback={<div>Loading...</div>}>
             <Header />
+            </Suspense>
             
             {children}
             <NavMenuMobile />
