ced03ea69502d91e43936bfeabb9b6928367d0dc
Kirill Mokhov <vxruuu@gmaIl.com>
2025-11-10 14:53:59 +0300

    suspence profile

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,10 +1,13 @@
 import Header from "@/components/frontend/header";
 import NavMenuMobile from "@/components/frontend/nav-menu-mobile";
+import { Suspense } from "react";
 
 export default function ProductsLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
+            <Suspense fallback={<div>Loading...</div>}>
             <Header />
+            </Suspense>
             <main className="min-h-screen bg-gray-100">{children}</main>
             <NavMenuMobile />
         </>
