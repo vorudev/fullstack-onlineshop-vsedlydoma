e2d996ab1738aef00f7a39ee762e8f3ffc48010f
Kirill Mokhov <vxruuu@gmaIl.com>
2025-11-12 20:45:46 +0300

    12121

--- app/products/add-to-cart-prop.tsx
+++ app/products/add-to-cart-prop.tsx
@@ -38,7 +38,7 @@ export const AddToCart: React.FC<ProductUnited> = ({ product }) => {
     
     return (
         <button
-        className={` lg:px-3 px-2 bg-blue-600 lg:min-w-[140px] text-[14px] lg:text-[16px] font-semibold lg:py-3 py-2 rounded-xl flex items-center justify-center gap-2 transition-colors ${isInCart ? "bg-white text-blue-600 border border-blue-600" : "cursor-pointer text-white hover:bg-blue-700  "}`}
+        className={` lg:px-3 px-2 bg-blue-600 lg:min-w-[140px] min-w-[120px] text-[14px] lg:text-[16px] font-semibold lg:py-3 py-2 rounded-xl flex items-center justify-center gap-2 transition-colors ${isInCart ? "bg-white text-blue-600 border border-blue-600" : "cursor-pointer text-white hover:bg-blue-700  "}`}
         onClick={() => addToCart(product)}
         disabled={isInCart}
         >
