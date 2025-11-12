ee0e529d5e9d2381780d5182550c05bde520f629
Kirill Mokhov <vxruuu@gmaIl.com>
2025-11-12 10:34:14 +0300

    Update auth-client.ts

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { adminClient, phoneNumberClient } from "better-auth/client/plugins"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000", 
+    baseURL: "https://fullstack-onlineshop-vsedlydoma.vercel.app", 
      plugins: [
         adminClient(),
         phoneNumberClient(),
