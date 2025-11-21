c18366f29693c2a2a25e65343dd175065a3ae3f3
Kirill Mokhov <vxruuu@gmaIl.com>
2025-11-21 20:37:55 +0300

    Update home.tsx

--- components/frontend/home.tsx
+++ components/frontend/home.tsx
@@ -1,4 +1,4 @@
-import { Eye, Heart, ShoppingCart, MapPin, MapPinned, Grid3x3, Building2, TextSelect  } from "lucide-react";
+import { Eye, Heart, ShoppingCart, MapPin, MapPinned, Grid3x3, PhoneCall, Building2, TextSelect  } from "lucide-react";
 import Map from "./map";
 import CategoriesTable from "../categories-table-user-2";
 import { getRandomProductsFast, getProducts } from "@/lib/actions/product";
@@ -38,7 +38,7 @@ const manufacturersWithImages = manufacturers?.map(manufacturer => {
 
     return (
       
-        <div className=" min-h-screen mx-auto  xl:max-w-[1400px] lg:max-w-[1000px]  text-black">
+        <div className=" min-h-screen mx-auto  pb-30 xl:max-w-[1400px] lg:max-w-[1000px]  text-black">
           <div className=" absolute z-10 hidden lg:flex"><CategoriesTable categories={categories} /></div>
           <div className="lg:ml-81 ml-0 overflow-hidden md:pt-10 lg:pt-5 pt-4 flex flex-col gap-7  md:px-10">
          
@@ -51,7 +51,7 @@ const manufacturersWithImages = manufacturers?.map(manufacturer => {
    <UserGreetingHome />
    <div className="min-w-[80vw] md:min-w-[40vw] lg:min-w-[20vw] md:w-full xl:w-[30%] xl:min-w-0 bg-blue-100 rounded-xl shadow py-4 pl-4 snap-center flex-col flex  justify-between lg:hidden gap-10 relative  overflow-hidden">
    <div className="flex flex-col">
-    <h3 className="text-lg xl:text-xl font-semibold ">Как нас найти</h3>
+    <h3 className="text-lg xl:text-xl font-semibold ">Как нас найти?</h3>
     <p className="text-gray-600 text-sm xl:text-base ">Информация о нашем магазине</p>
     </div> 
     <div className="flex">
@@ -115,7 +115,61 @@ const manufacturersWithImages = manufacturers?.map(manufacturer => {
         <SliderHome products={productsWithDetailAndImages || []} />
         
            </div>
-           
+           <div className="flex lg:hidden overflow-x-auto gap-4 px-4 md:px-0 snap-x snap-mandatory " style={{ 
+      scrollbarWidth: 'none',
+      msOverflowStyle: 'none',
+    }}>
+  
+  
+
+   <div className="min-w-[80vw] md:min-w-[40vw] lg:min-w-[20vw] md:w-full xl:w-[30%] xl:min-w-0 bg-orange-100 rounded-xl shadow py-4 pl-4 snap-center flex-col flex  justify-between lg:hidden gap-10 relative  overflow-hidden">
+   <div className="flex flex-col">
+    <h3 className="text-lg xl:text-xl font-semibold ">Есть вопрос? Свяжитесь с нами!</h3>
+    <p className="text-gray-600 text-sm xl:text-base ">Наши специалисты всегда готовы помочь и разобраться в вашем вопросе</p>
+    </div> 
+    <div className="flex">
+      <Link href="/about" className="bg-white rounded-lg   text-sm border border-gray-300 py-2 px-3 xl:text-base">Показать телефон</Link>
+      </div>
+    
+    <div className="absolute -bottom-4 -right-5 text-orange-500/50">
+    <PhoneCall className="w-30 h-30" />
+    </div>
+  </div>
+  <div className="min-w-[80vw] md:min-w-[40vw] lg:min-w-[20vw] md:w-full xl:w-[30%] xl:min-w-0 bg-blue-100 rounded-xl shadow py-4 pl-4 snap-center flex-col flex  justify-between lg:hidden gap-10 relative  overflow-hidden">
+   <div className="flex flex-col">
+    <h3 className="text-lg xl:text-xl font-semibold ">Как нас найти</h3>
+    <p className="text-gray-600 text-sm xl:text-base ">Информация о нашем магазине</p>
+    </div> 
+    <div className="flex">
+      <Link href="/about" className="bg-white rounded-lg   text-sm border border-gray-300 py-2 px-3 xl:text-base">Показать на карте</Link>
+      </div>
+    
+    <div className="absolute -bottom-4 -right-5 text-blue-500/50">
+    <MapPinned className="w-30 h-30" />
+    </div>
+  </div>
+    <Link href="/categories" className="w-[20%] xl:flex hidden bg-green-100/50 rounded-xl shadow py-4  px-3 flex-col gap-6 relative overflow-hidden">
+    <div className="flex flex-col">
+      <h3 className="text-base font-semibold xl:text-xl">Каталог</h3>
+      <p className="text-gray-600 text-sm xl:text-base">Все товары</p>
+    </div>
+    <div className="absolute -bottom-3 -right-3 text-green-500/50">
+      <TextSelect className="w-20 h-20" />
+    </div>
+  </Link>
+   <div className="flex-1 bg-purple-100 hidden  rounded-xl shadow py-4 pl-4 pr-2 xl:flex flex-col items-between justify-between gap-2 relative overflow-hidden">
+    <div className="flex flex-col ">
+      <h3 className="text-base font-semibold xl:text-xl">Производители</h3>
+      <p className="text-gray-600 text-sm xl:text-base">Популярные бренды</p>
+    </div>
+    <Link href="/manufacturers" className="bg-white rounded-lg text-sm border border-gray-300 py-2 px-3 w-fit xl:text-base">
+      Смотреть все
+    </Link>
+    <div className="absolute -bottom-4 -right-4 text-purple-500/50">
+      <Building2 className="w-24 h-24" />
+    </div>
+  </div>
+</div>
 
          <div className="bg-white rounded-2xl  p-8 mb-12 overflow-hidden  hidden lg:flex w-full ">
           <div className=" flex-col lg:flex-row gap-8 items-center flex w-full  ">
