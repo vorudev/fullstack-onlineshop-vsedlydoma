0a4c39285e03523673e4ff400be839ee3efabd9d
Kirill Mokhov <vxruuu@gmaIl.com>
2025-12-08 22:01:24 +0300

    1234

--- next.config.ts
+++ next.config.ts
@@ -9,14 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-    async rewrites() {
-    		return [
-    			{
-    				source: '/api/c15t/:path*',
-    				destination: `${process.env.NEXT_PUBLIC_C15T_URL}/:path*`,
-    			},
-    		];
-    	}
 };
 
 export default nextConfig;
