70fe8893c56ee94f372aab27dc707e42af65008b
Kirill Mokhov <vxruuu@gmaIl.com>
2025-12-13 06:49:09 +0300

    21221

--- app/2fa-enable/layout.tsx
+++ app/2fa-enable/layout.tsx
@@ -1,16 +1,17 @@
 import NavMenuMobile from "@/components/frontend/nav-menu-mobile";
 import Header from "@/components/frontend/header";
 import Footer from "@/components/frontend/footer";
-
-export default function MenuLayout({ children }: { children: React.ReactNode }) {
+import { getContactUs } from "@/lib/actions/contact-us";
+export default async function MenuLayout({ children }: { children: React.ReactNode }) {
+  const contacts = await getContactUs()
   return (
     <>
 
-      <Header />
+      <Header contacts={contacts} />
  <main className="bg-gray-100">
         {children}
            <NavMenuMobile />
-           <Footer />
+           <Footer contacts={contacts} />
            </main>
     </>
   );
