bb90fcaa762833d1c66cc51a5d508a0605a23422
Kirill Mokhov <vxruuu@gmaIl.com>
2025-12-25 15:18:08 +0300

    Update auth-client.ts

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { adminClient, phoneNumberClient, twoFactorClient, organizationClient } from "better-auth/client/plugins"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000", 
+    baseURL: "https://www.santehgorod.by", 
      plugins: [
         adminClient(),
         phoneNumberClient(),
